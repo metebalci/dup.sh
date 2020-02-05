b2bf12eb2df8f9aed68b3540cad2005ce0ee0f08
Mete Balci <metebalci@gmail.com>
2020-02-05 16:25:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
+
 # dup.sh
-duplicate file processor script
+
+*WARNING: This tool uses mv and rm commands, that may modify the file system, move and delete files. Use it with extra caution.* 
+
+dup.sh is a bash script to find and optionally move or delete duplicate files.
+
+Its functionally is similar to [fdupes](https://github.com/adrianlopezroche/fdupes) with some differences:
+
+- dup.sh saves the intermediate processing state. This is the main reason I created this script because when I want to run fdupes over a huge number of files spread across many folders in a network share, it takes a lot of time and if something happens (if I terminate the execution etc.), it has to restart, there is no way to save the intermediate state. Since dup.sh saves the intermediate state, it restarts the process from where it stopped and that is particularly useful while calculating the hashes, the most computationally heavy part of the process.
+
+- dup.sh uses sha1 by default. It uses [shasum](https://linux.die.net/man/1/shasum) tool and you can easily change this to something else. I think the performance difference between md5 and sha1 is minimal in modern computers so I decided to use sha1.
+
+- dup.sh uses the awesome [GNU Parallel](https://www.gnu.org/software/parallel/) tool to distribute the calculation of hashes to the cores.
+
+- Because dup.sh is a shell script, it has minimal dependencies and it does not need compilation.
+
+- dup.sh can both delete and move duplicate files.
+
+- dup.sh always run in recursive mode, it always goes into subdirectories, however it does not follow symbolic links.
+
+# Compatibility
+
+I am using the script in macOS. Since it is a shell script, it should run on Linux and probably in Windows Subsystem for Linux, maybe with a slight modification.
+
+# Usage
+
+Run dup.sh to see the usage. Basic commands:
+
+- dups.sh clean: cleans intermediate and temporary files
+- dups.sh prepare: prepare intermediate files, required for move and delete
+- dups.sh move: move duplicate files (leaving only one copy) to another folder keeping their directory structure, use testmove to see move commands instead of executing
+- dups.sh delete: delete duplicate files (leaving only one copy), use testdelete to see rm commands instead of executing
+
+Below is the output from a test run while running on Linux kernel repo.
+
+![dup.sh screenshot](dupsh.png?raw=true)
+
+# Performance
+
+I cloned [Linux kernel repo at Github](https://github.com/github/linux). Then I run `fdupes -mr .` and `dup.sh prepare` on a Mac Mini 2018 with Intel i7 six-core CPU. gtime is GNU time.
+
+fdupes:
+
+```
+$ gtime fdupes -mr .
+412 duplicate files (in 240 sets), occupying 1.7 megabytes
+
+0:11.68 real
+1.66 user
+9.94 sys
+0 amem
+11868 mmem
+```
+
+dup.sh:
+
+```
+$ gtime dup.sh prepare
+finding duplicate_files
+creating file_list
+.number of files: 62893
+calculating file_hashes
+.calculating hashes of all files
+100% 62893:0=0s ./virt/lib/irqbypass.c                                                         
+finding duplicate_hashes
+8:55.90 real
+1381.11 user
+906.31 sys
+0 amem
+50908 mmem
+```
+
+As you see in this example dup.sh is very slow. So the point of using it should not be performance but other factors as summarized in the description.
+
+# License
+
+Copyright (C) 2020 Mete Balci
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ dup.sh
@@ -0,0 +1,307 @@
+#!/bin/bash
+
+clean() {
+	
+	rm -f .dup.file_list .dup.file_hashes .dup.duplicate_hashes .dup.duplicate_files .dup.remaining_files
+
+}
+
+file_list () {
+
+	if [ -f .dup.file_list ]; then
+
+		echo ".reusing existing file_list"
+
+	else
+
+		echo "creating file_list"
+
+		# processes only regular files (type f)
+		find -sx . -type f > .dup.file_list
+
+	fi
+
+	echo ".number of files: $(wc -l .dup.file_list | xargs | cut -f1 -d' ')"
+
+}
+
+file_hashes () {
+
+	file_list
+
+	echo "calculating file_hashes"
+
+	if [ -f .dup.file_hashes ]; then
+
+		echo ".using existing file_hashes to find what is missing and only calculating their hashes"
+
+		# combine existing file names in .dup.file_hashes and .dup.file_list
+		# sort and find uniq ones (so it only exists in .dup.file_list)
+		# calculate hashes only for that files and append to .dup.file_hashes
+		cut -c43- .dup.file_hashes | cat .dup.file_list - | sort | uniq -u > .dup.remaining_files
+
+		echo ".number of hashes missing to calculate: $(wc -l .dup.remaining_files | xargs | cut -f1 -d' ')"
+
+		cat .dup.remaining_files | parallel --bar -I% --max-args 1 shasum % ">>" .dup.file_hashes
+
+		# exit here if there is a problem in paralel execution
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
+
+	else
+
+		echo ".calculating hashes of all files"
+
+		# create an empty file
+		touch .dup.file_hashes
+
+		# calculates sha1 for every file in file list in parallel
+		cat .dup.file_list | parallel --bar -I% --max-args 1 shasum % ">>" .dup.file_hashes
+
+		# exit here if there is a problem in paralel execution
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
+
+	fi
+
+}
+
+duplicate_hashes () {
+
+	if [ -f .dup.duplicate_hashes ]; then
+
+		echo ".using existing duplicate_hashes"
+
+	else
+
+		file_hashes
+
+		echo "finding duplicate_hashes"
+
+		# finds duplicate hashes, unique files are excluded from processing here
+		sort .dup.file_hashes | cut -c-40 | uniq -d > .dup.duplicate_hashes
+		
+	fi
+
+}
+
+duplicate_files () {
+
+	if [ -f .dup.duplicate_files ]; then
+
+		echo ".using existing duplicate_files"
+
+	else 
+
+		echo "finding duplicate_files"
+
+		duplicate_hashes
+		
+		# reseting the file first because only appending below
+		rm -f .dup.duplicate_files
+		touch .dup.duplicate_files
+
+		cat .dup.duplicate_hashes | while read hash; do
+
+			# append the files having same hash
+			grep $hash .dup.file_hashes | cut -c43- >> .dup.duplicate_files
+			# append an empty line
+			echo "" >> .dup.duplicate_files
+
+		done
+
+	fi
+
+}
+
+move () {
+
+	# true if real move requested
+	mode=$1
+
+	# the move folder should not already exist
+	if [ -d .dup.moved_files ]; then
+		echo "error: .dup.moved_files already exists. please remove it to do another move."
+		exit 1
+	fi
+
+	file_hashes
+	duplicate_hashes
+
+	# create the move folder if real move requested
+	if $mode; then
+		mkdir .dup.moved_files
+	fi
+
+	# for each hash
+	cat .dup.duplicate_hashes | while read hash; do
+
+		first=true
+
+		# find the files with this hash value
+		grep $hash .dup.file_hashes | cut -c43- | while read file; do
+
+			# omit the first file
+			if $first; then
+				first=false
+				continue
+			fi
+
+			# find the dir and base of file
+			dir=$(dirname $file)
+			base=$(basename $file)
+
+			if $mode; then
+
+				# create the intermediate directory
+				mkdir -p .dup.moved_files/$dir
+				# move the file
+				mv $file .dup.moved_files/$dir/$base
+
+			else
+
+				# just print the potential move command
+				echo "mv $file .dup.moved_files/$dir/$base"
+
+			fi
+
+		done
+
+	done
+
+}
+
+delete () {
+
+	# true if real delete requested
+	mode=$1
+
+	file_hashes
+	duplicate_hashes
+
+	# for each hash
+	cat .dup.duplicate_hashes | while read hash; do
+
+		first=true
+
+		# find the files with this hash value
+		grep $hash .dup.file_hashes | cut -c43- | while read file; do
+
+			# omit the first file
+			if $first; then
+				first=false
+				continue
+			fi
+
+			if $mode; then
+
+				rm -f $file
+
+			else
+
+				# just print the potential delete command
+				echo "rm -f $file"
+
+			fi
+
+		done
+
+	done
+
+}
+
+if [ ! -x "$(command -v shasum)" ]; then
+	echo "error: please install shasum program."
+	exit 1
+fi
+
+if [ ! -x "$(command -v parallel)" ]; then
+	echo "error: please install gnu parallel program."
+	exit 1 
+fi
+
+case "$1" in
+
+	clean)
+		clean
+		;;
+
+	file_list)
+		file_list
+		;;
+
+	file_hashes)
+		file_hashes
+		;;
+
+	duplicate_hashes)
+		duplicate_hashes
+		;;
+
+	duplicate_files)
+		duplicate_files
+		;;
+
+	prepare)
+		duplicate_files
+		;;
+
+	testmove)
+		move false
+		;;
+
+	move)
+		move true
+		;;
+
+	testdelete)
+		delete false
+		;;
+
+	delete)
+		delete true
+		;;
+
+	*)
+		echo ""
+		echo "dup.sh: finds duplicate regular files in folders, uses sha1 and gnu parallel"
+		echo "				optionally moves all duplicate files but one to a folder"
+		echo ""
+		echo "description: dups.sh it runs in stages:"
+		echo "				1) creates file list: output .dup.file_list"
+		echo "				2) calculates file hashes: output .dup.file_hashes"
+		echo "				3) finds duplicate hashes: output .dup.duplicate_hashes"
+		echo "				4) finds duplicate files: output .dup.duplicate_files"
+		echo "				the order is like this but you can delete individual files and they will be recreated using previous files as it is."
+		echo "				.dup.file_list and .dup.file_hashes are always compared, and if a difference is found, these hashes arecalculated."
+		echo "				move (and test) uses only .dup.duplicate_hashes"
+		echo ""
+		echo "usage: dup.sh clean|prepare|test|move"
+		echo ""
+		echo "	clean: removes the dup processing and temporary files, but not moved files folder"
+		echo "	prepare: creates dup processing files, it can be used to break the processing into calculating hashes and actually moving files."
+		echo "	test: shows the mv commands, does not execute them, nothing is moved, creates processing files if needed"
+		echo "	move: actually moves all duplicate files but one, creates processing files if needed"
+		echo ""
+		echo "	processing files are created if they do not exist, this allows partial processing. the files are:"
+		echo ""		 
+		echo "	- .dup.file_list: list of all regular files in all subfolders"
+		echo "	- .dup.file_hashes: (sha1) hashes of all files in file_list"
+		echo "	- .dup.duplicate_hashes: list of duplicate hashes in file_hashes"
+		echo "	- .dup.duplicate_files: list of duplicate files grouped and groups are separeted by one empty line"
+		echo ""
+		echo "	recommended use:"
+		echo "		- run dup.sh clean"
+		echo "		- run dup.sh prepare, and check .dup.duplicate_files"
+		echo "		- run dup.sh move, and check files remained and .dup.moved_files folder"
+		echo "		- delete .dup.moved_files folder if you are sure all are OK"
+		echo ""
+		echo "  temporary files:"
+		echo "	- dup.remaining_files"
+		echo ""
+		echo "  it is possible to run each stage individually by file_list, file_hashes, duplicate_hashes, duplicate_files commands"
+		echo ""
+
+esac
